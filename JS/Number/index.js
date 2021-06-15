22e07dc5e4c5b7108af83a8ada3fb8a35d9caa40
kawano108 <tk108gabalian@gmail.com>
2021-06-16 00:38:25 +0900

    make number js file

---
+++ JS/Number/index.js
@@ -0,0 +1,13 @@
+// JSには一つの数値型しかない。それがnumber型
+
+console.log(.1 + .2) // 0.30000000000000004
+
+// 無限
+
+console.log(Number.MAX_VALUE) // 1.7976931348623157e+308
+
+console.log( 1 / 0) // Infinity
+console.log(-1 / 0) // -Infinity
+
+console.log( Infinity >  1) // true
+console.log(-Infinity < -1) // true
