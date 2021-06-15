be1d75c89d344366afdef96f5ca988c921bddb35
kawano108 <tk108gabalian@gmail.com>
2021-06-15 19:54:48 +0900

    make equality js file

---
+++ JS/Equality/index.js
@@ -0,0 +1,9 @@
+
+console.log(5 == "5") // true
+console.log(5 === "5") // true
+
+console.log("" == "0") // false
+console.log(0 == "") // true 0もEmptyもfalseのように振る舞うという点で共通だからとのこと
+
+console.log("" === "0") // false
+console.log(0 === "") // false
